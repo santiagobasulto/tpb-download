409c3859ea358b2e109a2b78a708e253c2e72d97
Santiago Basulto <santiago.basulto@gmail.com>
2012-12-19 16:14:55 -0300

    Initial commit with code

---
+++ tpb.py
@@ -0,0 +1,41 @@
+import urllib2
+import argparse
+
+TPB_TORRENT_URL = "http://torrents.thepiratebay.se"
+
+
+def torrent_download_url(torrent_id):
+    url = '%s/%s' % (TPB_TORRENT_URL, torrent_id)
+    return url
+
+
+def download_torrent(torrent_id):
+    url = torrent_download_url(torrent_id)
+    u = urllib2.urlopen(url)
+    torrent_file_name = '%s.torrent' % torrent_id
+    torrent_file = open(torrent_file_name, 'w')
+    print "Downloading %s.torrent" % torrent_id
+    torrent_file.write(u.read())
+    torrent_file.close()
+
+
+def main(torrent_ids):
+    print "Starting to download torrent files."
+    for tid in torrent_ids:
+        download_torrent(tid)
+    print "Done downloading torrents."
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Downloads one or more torrent files from ThePirateBay.org'
+    )
+    parser.add_argument(
+        'torrent_ids',
+        metavar='Torrent-ID',
+        type=int,
+        nargs='+',
+        help='One or more Torrent IDs to download'
+    )
+
+    args = parser.parse_args()
+    main(args.torrent_ids)
